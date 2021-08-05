555892858938ee1a3fee30e7b0a82a7a827220ca
Iván GalazJeria <dequin@gmail.com>
2021-08-05 12:06:22 +0200

    Create the Feed Item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Iván GalazJeria on 05-08-21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
