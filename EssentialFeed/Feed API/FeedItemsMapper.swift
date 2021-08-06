0887dd7cb02fe82ba5b05f83b2cb1585daa1ccc1
Iván GalazJeria <dequin@gmail.com>
2021-08-06 19:13:31 +0200

    Move all mapping login to the FeedItemMapper

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -11,6 +11,8 @@ internal final class FeedItemsMapper {
     
     private struct Root: Decodable {
         let items: [Item]
+        
+        var feed: [FeedItem] { items.map { $0.item } }
     }
 
     private struct Item: Decodable {
@@ -30,15 +32,14 @@ internal final class FeedItemsMapper {
     }
     
     private static var OK_200: Int { 200 }
-    
-    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
+
+    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
         
-        return try JSONDecoder()
-            .decode(Root.self, from: data)
-            .items
-            .map { $0.item }
+        guard response.statusCode == OK_200,
+              let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(.invalidData)
+        }
+
+        return .success(root.feed)
     }
 }
