1856b9629b386d7f924a2562fff79a2b5465d3a9
Iván GalazJeria <dequin@gmail.com>
2021-08-15 20:55:43 +0200

    Extract system under test creation to a factory method to protect tests against breaking changes

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -10,14 +10,14 @@ import EssentialFeed
 
 class LocalFeedLoader {
     private let store: FeedStore
-
-     init(store: FeedStore) {
-         self.store = store
-     }
-
-     func save(_ items: [FeedItem]) {
-         store.deleteCachedFeed()
-     }
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
 }
 
 class FeedStore {
@@ -31,28 +31,32 @@ class FeedStore {
 final class CacheFeedUseCaseTests:XCTestCase {
     
     func test() {
-        let store = FeedStore()
-        _ = LocalFeedLoader(store: store)
+        let (_, store) = makeSUT()
+        
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
     
     func test_save_requestsCacheDeletion() {
-         let store = FeedStore()
-         let sut = LocalFeedLoader(store: store)
-         let items = [uniqueItem(), uniqueItem()]
-
-         sut.save(items)
-
-         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
-     }
-
-     // MARK: - Helpers
-     
-     private func uniqueItem() -> FeedItem {
-         return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
-     }
-
-     private func anyURL() -> URL {
-         return URL(string: "http://any-url.com")!
-     }
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT()
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        return (sut, store)
+    }
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
 }
