2b31068d39fa108278ccddbd956e951fc1764344
Iván GalazJeria <dequin@gmail.com>
2021-08-16 08:34:40 +0200

    Remove references of `items` in favor of `images` which is a domain term used by domain experts in the specs

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -143,19 +143,19 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         .failure(error)
     }
     
-    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
+    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedImage, json: [String: Any]) {
         
-        let item = FeedItem(
+        let item = FeedImage(
             id: id,
             description: description,
             location: location,
-            imageURL: imageURL)
+            url: imageURL)
         
         let json = [
             "id": item.id.uuidString,
             "description": item.description,
             "location": item.location,
-            "image": item.imageURL.absoluteString
+            "image": item.url.absoluteString
         ].compactMapValues { $0 }
         
         return (item, json)
