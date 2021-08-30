26a814a429ea2f1f1a69275435f84656ad2f6033
Iván GalazJeria <dequin@hey.com>
2021-08-30 08:50:49 +0200

    Remove Base translation, Fix Feed store reference. Add test for all keys to have values in all languages

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -16,7 +16,7 @@ protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
 
-final class FeedPresenter {
+public final class FeedPresenter {
     private let feedView: FeedView
     private let loadingView: FeedLoadingView
     
