7b417580a2ac97ef16567acd684e9afbc8af212f
Iván GalazJeria <dequin@hey.com>
2021-08-30 07:43:30 +0200

    Extract `FeedViewController` creation and configuration into a factory method. Extract `FeedRefreshViewController` creation and configuration into a factory method.

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -14,18 +14,34 @@ public final class FeedUIComposer {
     public static func feedComposedWith(loader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: loader)
         
-        let bundle = Bundle(for: FeedViewController.self)
-        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
-        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-        let refreshController = feedController.refreshController!
-        refreshController.delegate = presentationAdapter
-        feedController.title = FeedPresenter.title
-        
+        let feedController = FeedViewController.makeWith(title: FeedPresenter.title)
+        let refreshController = FeedRefreshViewController.makeFrom(feedController, with: presentationAdapter)
+
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
             loadingView: WeakRefVirtualProxy(refreshController)
         )
         return feedController
+    }
+}
+
+private extension FeedViewController {
+    static func makeWith(title: String) -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+
+        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+        feedController.title = title
+                
+        return feedController
+    }
+}
+
+private extension FeedRefreshViewController {
+    static func makeFrom(_ feedController: FeedViewController, with delegate: FeedLoaderPresentationAdapter) -> FeedRefreshViewController{
+        let refreshController = feedController.refreshController!
+        refreshController.delegate = delegate
+        return refreshController
     }
 }
 
