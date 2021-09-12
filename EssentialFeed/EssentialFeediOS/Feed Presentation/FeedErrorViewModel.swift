c2d0867b9a2474f1288dc7b691a9b1d1cea4e1c1
Iván GalazJeria <dequin@hey.com>
2021-09-12 12:53:02 +0200

    Add `FeedErrorViewModel` factory helpers to clarify intent in the point of use

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -9,5 +9,13 @@ import Foundation
 
 struct FeedErrorViewModel {
     let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        FeedErrorViewModel(message: message)
+    }
 }
 
