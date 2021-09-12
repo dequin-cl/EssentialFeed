0ca8b2ffb0e1bf53e9beb58f8b86b41e19bc9f29
Iván GalazJeria <dequin@hey.com>
2021-09-12 20:29:38 +0200

    Add makeSUT

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -17,13 +17,18 @@ struct RemoteFeedImageDataLoader {
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
     
     func test_init_doesNotPerformAnyURLRequest() {
-        let client = HTTPClientSpy()
-        let _ = RemoteFeedImageDataLoader(client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     // MARK:- Helpers
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut:RemoteFeedImageDataLoader, client:HTTPClientSpy){
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
     
     private class HTTPClientSpy {
         var requestedURLs = [URL]()
